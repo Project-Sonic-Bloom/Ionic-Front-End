2593f1588b97d0c0ce8cd562774402da52f510f7
G00401808@atu.ie <G00401808@atu.ie>
2024-12-05 14:29:24 +0000

    added drag drop zone to the form

--- Sonic-Bloom-Client/src/app/components/form/form.component.ts
+++ Sonic-Bloom-Client/src/app/components/form/form.component.ts
@@ -11,19 +11,48 @@ import { HttpClientService } from 'src/app/services/http-service/http-client.ser
 })
 export class FormComponent {
 
+  // storing files
   selectedFile: File | null = null;
   backEnd: string = "http://127.0.0.1:5000/"
+
+  // trigger when drag DOM events happen
+  isDragOver = false;
 
 
   constructor(private httpClientService: HttpClientService) { }
 
   onSubmit(event: Event) {
-    event.preventDefault();//stop the submit from refreshing the page (debug purpose)
+    event.preventDefault();// stop the submit from refreshing the page (debug purpose)
     // Use subscribe to access observable because it allows for the asynchronous processing required of HttpClient's get method/observable that is returned
     this.httpClientService.get(this.backEnd).subscribe(
       (response) => {
         console.log('Data received:', response);
       })
+  }
+
+  onDragOver(event: Event) {
+    
+    // stop unintended behaviours
+    event.preventDefault();
+
+    // stops bubbling actions to parent elements of the event that would lead to unintended behaviour
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Examples#example_5_event_propagation
+    event.stopPropagation();
+
+    //changes css mainly
+    this.isDragOver = true;
+  }
+
+  onDragLeave(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragOver = false;
+  }
+
+  onDrop(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragOver = false;
   }
 
 }
