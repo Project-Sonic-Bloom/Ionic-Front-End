364c82d831e3767bf99265e07ffb8809eaec5be1
G00401808@atu.ie <G00401808@atu.ie>
2024-12-06 01:04:24 +0000

    set up canvas to layer over the map to its exact area on page, set up canvas for editing via map.component.ts

--- Sonic-Bloom-Client/src/app/components/map/map.component.ts
+++ Sonic-Bloom-Client/src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss'],
   standalone: true,
 })
-export class MapComponent  implements OnInit {
+export class MapComponent {
+  // really helpful tutorial that helped me gain a deeper understanding of how the @ViewChild decorator & ElementRef class can be used 
+  // to get/use an element from the template.
+  // https://www.youtube.com/watch?v=uoLzWRZgXiA
+  // get the canvas element
+  @ViewChild('canvasOverlay') canvasOverlay!: ElementRef<HTMLCanvasElement>;
 
   constructor() { }
 
-  ngOnInit() {}
+  // obviously a hook method angular provides https://angular.dev/api/core/AfterViewInit
+  ngAfterViewInit() {
+    this.initCanvas();
+  }
+
+  initCanvas() {
+    // canvas is native to html not typescript so to edit a html elements properties in angular the code below is needed
+    // Same code but in a javascript example: const canvas = document.getElementById("canvasOverlay");
+    // https://tutorialscamp.com/angular-viewchild/
+    const canvas = this.canvasOverlay.nativeElement;
+
+    // canvas logic below found from last year's graphics programming module
+    const ctx = canvas.getContext('2d');
+  }
+
 
 }
