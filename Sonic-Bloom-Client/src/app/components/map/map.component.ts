246decb3cb92da9dd24395eed5006c17e077154e
G00401808@atu.ie <G00401808@atu.ie>
2025-04-25 23:41:30 +0100

    Made very necessary code cleanup to the map component. (removed nonfunctioning html2canvas screenshotting as it was an extra dependency)

--- Sonic-Bloom-Client/src/app/components/map/map.component.ts
+++ Sonic-Bloom-Client/src/app/components/map/map.component.ts
@@ -2,7 +2,6 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { HttpClientService } from 'src/app/services/http-service/http-client.service';
 import { environment } from '../../../environments/environment.prod';
-import html2canvas from 'html2canvas';
 import { GoogleMap, GoogleMapsModule } from '@angular/google-maps';
 import { NgFor } from '@angular/common';
 
@@ -21,21 +20,12 @@ export class MapComponent {
   // https://www.youtube.com/watch?v=uoLzWRZgXiA
   // get the canvas element
   @ViewChild(GoogleMap) googleMap!: GoogleMap;//for typescript abilities
-  @ViewChild('canvasOverlay') canvasOverlay!: ElementRef<HTMLCanvasElement>;
   @ViewChild('map') map!: ElementRef<HTMLDivElement>;
 
   backEnd: string = "http://127.0.0.1:5000/";
-  // https://v17.angular.io/guide/security#xss sanitize the url to bypass security concerns due to the maps key being a variable
-  // map_url = this.domSanitizer.bypassSecurityTrustResourceUrl("https://www.google.com/maps/embed/v1/view?key=" + environment.maps_key + "&center=53.4297953,-8.4111406&maptype=satellite&zoom=7");
-  // map_url = this.domSanitizer.bypassSecurityTrustResourceUrl(environment.maps_key);
 
-  // map paramaters
-  // center: google.maps.LatLngLiteral = { lat: 53.4356987, lng: -8.5634334 };
-  // zoom = 7;
-  // mapTypeId = google.maps.MapTypeId.HYBRID;
-  // markers: any[] = [];
+  // Make the map
   marker: any;
-
   options: google.maps.MapOptions = {
     mapTypeId: google.maps.MapTypeId.HYBRID,
     mapId: '13f868347395f4d9',
@@ -44,20 +34,9 @@ export class MapComponent {
     disableDefaultUI: false
   };
 
-  // obviously a hook method angular provides https://angular.dev/api/core/AfterViewInit
+  // hook method angular provides https://angular.dev/api/core/AfterViewInit
   ngAfterViewInit() {
-    this.initCanvas();
     this.initMap();
-  }
-
-  initCanvas() {
-    // canvas is native to html not typescript so to edit a html elements properties in angular the code below is needed
-    // Same code but in a javascript example: const canvas = document.getElementById("canvasOverlay");
-    // https://tutorialscamp.com/angular-viewchild/
-    const canvas = this.canvasOverlay.nativeElement;
-
-    // canvas logic below found from last year's graphics programming module
-    const ctx = canvas.getContext('2d');
   }
 
   initMap() {
@@ -96,7 +75,7 @@ export class MapComponent {
     // Use the Maps API to convert point to lat/lng
     const point = new google.maps.Point(x, y);
     
-    // We need to use the overlay projection
+    // Use the OverlayView method to ensure changes to the map are made accurate
     const overlay = new google.maps.OverlayView();
     overlay.setMap(this.googleMap.googleMap!);
     
@@ -134,60 +113,11 @@ export class MapComponent {
   }
 
   async screenshotMap() {
-    const map = this.map.nativeElement;
-
-    html2canvas(map).then((canvas) => {
-      // Convert canvas to an image
-      const image = canvas.toDataURL('image/png');
-
-      // Trigger download or preview the image
-      const link = document.createElement('a');
-      link.href = image;
-      link.download = 'map-screenshot.png';
-      link.click();
-    }).catch((error) => {
-      console.error('Error capturing the map:', error);
-    });
-
-
-    // this.saveMap();
-    // console.log("sent get")
-    // //send a get request to let the server know it has to take a screenshot
-    // this.httpClientService.get(this.backEnd + "GetScreenshot").subscribe(
-    //   (response) => {
-    //     console.log(response);
-    //   }
-    // )
+    
   }
 
-  // Function to save the map/canvas div state in cookies
-  // This is done by extracting a link from the iframe that contains the co-ordinates
   saveMap(): void {
 
-    // DOES NOT WORK DUE TO CORS ERROR
-
-    // // iterate over all of the elements in the iframe
-    // document.querySelectorAll("iframe").forEach((iframe, index) => {
-    //   // console.log("here")
-    //   try {
-    //     // ensure iframe exists
-    //     const iframeDocument = (iframe as HTMLIFrameElement).contentDocument;
-    //     console.log(`Found ${iframeDocument} iframe`);
-    //     if (iframeDocument) {
-    //       console.log("here")
-
-    //       // find the anchor tag that has the link
-    //       const anchor = iframeDocument.querySelector('.google-maps-link a');
-    //       if (anchor && anchor instanceof HTMLAnchorElement) {
-    //         const url = anchor.href;
-    //         console.log(`URL: ${url}`);
-    //         // You can store the URL in an array or perform another action
-    //       }
-    //     }
-    //   } catch (error) {
-    //     console.error(`Error accessing iframe ${index}:`, error);
-    //   }
-    // });
   }
 
 
